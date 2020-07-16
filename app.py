08ea9465a37753f25e35fa6fea82ba3a339906b9
rkasale28 <rohitkasale28@gmail.com>
2020-07-16 22:23:45 +0530

    Done with Nation wise data

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 st.title("Covid Data Visualization")
 st.sidebar.title("Covid Data Visualization")
@@ -17,5 +20,33 @@ def load_data():
     return data, states
 
 data, states = load_data()
-st.write(data)
-st.write(states)
+modified_data = data.loc[data['State_Name'] == 'Total'].sort_values(by = ['Date'])
+st.subheader("Daily Updates")
+fig = go.Figure()
+options = ['Confirmed','Deceased','Recovered']
+st.sidebar.subheader("Daily Updates")
+choice = st.sidebar.multiselect('',options)
+if (len(choice)>0):
+    for ch in choice:
+        fig.add_trace(go.Line(x=modified_data['Date'], y=modified_data[ch],name = ch))
+else:
+    for option in options:
+        fig.add_trace(go.Line(x=modified_data['Date'], y=modified_data[option],name = option))
+
+fig.update_layout(
+    autosize=False,
+    width=800,
+    height=500,
+    margin=dict(
+        l=0,
+        r=0,
+        b=0,
+        t=0,
+        pad=0
+    ),
+)
+
+st.plotly_chart(fig)
+
+if (st.sidebar.checkbox("Show Data",False)):
+    st.write(modified_data)
