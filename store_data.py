50246c6d6c2899a52e2b3739fde3d90fd81ffe46
rkasale28 <rohitkasale28@gmail.com>
2020-07-21 22:47:20 +0530

    Removed store_district_data.py

--- store_data.py
+++ store_data.py
@@ -80,6 +80,33 @@ if not (df.empty):
 
     df = pd.DataFrame(new_json)
     df.to_csv(DATA_URL, mode='a', header=False, index=False)
-    print ('Successful')
+    print ('State Level Data: Successful')
 else:
-    print ('Nothing to add')
+    print ('State Level Data: Nothing to add')
+
+# District Level data
+resp = requests.get("https://api.covid19india.org/state_district_wise.json")
+resp = resp.json()
+
+DATA_URL = (
+"C:/Users/Rohit/Documents/Self Learning/Streamlit/district_level.csv"
+)
+
+new_json=[]
+
+for i in resp.keys():
+    districtData = resp[i]['districtData']
+    for j in districtData:
+        new_obj = {
+         'State_Name': i,
+         'District_Name':j,
+         'Active': districtData[j]['active'],
+         'Confirmed': districtData[j]['confirmed'],
+         'Deceased': districtData[j]['deceased'],
+         'Recovered': districtData[j]['recovered']
+        }
+        new_json.append(new_obj)
+
+df = pd.DataFrame(new_json)
+df.to_csv(DATA_URL, index=False)
+print ('District Level Data: Successful')
