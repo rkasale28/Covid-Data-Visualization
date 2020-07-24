7d925d4942be33cce2b8be9cc76d5caabf509b74
rkasale28 <rohitkasale28@gmail.com>
2020-07-24 09:21:05 +0530

    Minor Changes

--- store_data.py
+++ store_data.py
@@ -46,68 +46,74 @@ states = {'an':'Andaman and Nicobar Islands',
 
 # State Level Daily
 resp = requests.get("https://api.covid19india.org/states_daily.json")
-resp = resp.json()
+if (resp.status_code == 200):
+    resp = resp.json()
 
-df = pd.DataFrame(resp['states_daily'])
-df['date'] = pd.to_datetime(df['date'])
-df = df.sort_values(by=['date','status'])
+    df = pd.DataFrame(resp['states_daily'])
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.sort_values(by=['date','status'])
 
-DATA_URL = (
-"C:/Users/Rohit/Documents/Self Learning/Streamlit/state_level_daily.csv"
-)
+    DATA_URL = (
+    "C:/Users/Rohit/Documents/Self Learning/Streamlit/state_level_daily.csv"
+    )
 
-data = pd.read_csv(DATA_URL)
-data['Date'] = pd.to_datetime(data['Date'])
-data = data.sort_values(by=['Date'])
-data = data.iloc[-1:]
-dt = data['Date'].values[0]
+    data = pd.read_csv(DATA_URL)
+    data['Date'] = pd.to_datetime(data['Date'])
+    data = data.sort_values(by=['Date'])
+    data = data.iloc[-1:]
+    dt = data['Date'].values[0]
 
-df = df[df['date']>dt]
-df = df.rename(columns=states)
-df = df.set_index(['date','status'])
-df = df.T
-new_json = []
-if not (df.empty):
-    for (item,row) in df.iterrows():
-        date = datetime.strftime(row.index.get_level_values(0)[0],'%d-%b-%y')
-        new_obj = {
-            'Date' : date,
-            'Confirmed' : row[date]['Confirmed'],
-            'Deceased' :row[date]['Deceased'],
-            'Recovered': row[date]['Recovered'],
-            'State_Name': item
-        }
-        new_json.append(new_obj)
+    df = df[df['date']>dt]
+    df = df.rename(columns=states)
+    df = df.set_index(['date','status'])
+    df = df.T
+    new_json = []
+    if not (df.empty):
+        for (item,row) in df.iterrows():
+            date = datetime.strftime(row.index.get_level_values(0)[0],'%d-%b-%y')
+            new_obj = {
+                'Date' : date,
+                'Confirmed' : row[date]['Confirmed'],
+                'Deceased' :row[date]['Deceased'],
+                'Recovered': row[date]['Recovered'],
+                'State_Name': item
+            }
+            new_json.append(new_obj)
 
-    df = pd.DataFrame(new_json)
-    df.to_csv(DATA_URL, mode='a', header=False, index=False)
-    print ('State Level Data: Successful')
+        df = pd.DataFrame(new_json)
+        df.to_csv(DATA_URL, mode='a', header=False, index=False)
+        print ('State Level Data: Successful')
+    else:
+        print ('State Level Data: Nothing to add')
 else:
-    print ('State Level Data: Nothing to add')
+    print ('HTTP ERROR')
 
 # District Level data
 resp = requests.get("https://api.covid19india.org/state_district_wise.json")
-resp = resp.json()
+if (resp.status_code == 200):
+    resp = resp.json()
 
-DATA_URL = (
-"C:/Users/Rohit/Documents/Self Learning/Streamlit/district_level.csv"
-)
+    DATA_URL = (
+    "C:/Users/Rohit/Documents/Self Learning/Streamlit/district_level.csv"
+    )
 
-new_json=[]
+    new_json=[]
 
-for i in resp.keys():
-    districtData = resp[i]['districtData']
-    for j in districtData:
-        new_obj = {
-         'State_Name': i,
-         'District_Name':j,
-         'Active': districtData[j]['active'],
-         'Confirmed': districtData[j]['confirmed'],
-         'Deceased': districtData[j]['deceased'],
-         'Recovered': districtData[j]['recovered']
-        }
-        new_json.append(new_obj)
+    for i in resp.keys():
+        districtData = resp[i]['districtData']
+        for j in districtData:
+            new_obj = {
+             'State_Name': i,
+             'District_Name':j,
+             'Active': districtData[j]['active'],
+             'Confirmed': districtData[j]['confirmed'],
+             'Deceased': districtData[j]['deceased'],
+             'Recovered': districtData[j]['recovered']
+            }
+            new_json.append(new_obj)
 
-df = pd.DataFrame(new_json)
-df.to_csv(DATA_URL, index=False)
-print ('District Level Data: Successful')
+    df = pd.DataFrame(new_json)
+    df.to_csv(DATA_URL, index=False)
+    print ('District Level Data: Successful')
+else:
+    print ('HTTP ERROR')
